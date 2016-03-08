3b69ca8bc83a58e361dcf52e1bd4b5dc7f81eea6
Kiran Challa <kichalla@microsoft.com>
2016-03-07 16:07:12 -0800

    Initial

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Map("/getdata", appBuilder =>
+            {
+                appBuilder.Run(httpContext =>
+                {
+                    httpContext.Response.Cookies.Append(
+                        ".AspNetCore.Session",
+                    "RAExEmXpoCbueP_QYM",
+                    new CookieOptions() { HttpOnly = true, Path = "/" });
+
+                    httpContext.Response.Cookies.Append(
+                        ".AspNetCore.Antiforgery.Xam7_OeLcN4",
+                    "CfDJ8NGNxAt7CbdClq3UJ8_6w_4661wRQZT1aDtUOIUKshbcV4P0NdS8klCL5qGSN-PNBBV7w23G6MYpQ81t0PMmzIN4O04fqhZ0u1YPv66mixtkX3iTi291DgwT3o5kozfQhe08-RAExEmXpoCbueP_QYM",
+                    new CookieOptions { HttpOnly = true, Path = "/" });
+
+                    return httpContext.Response.WriteAsync("From getdata");
+                });
+            });
+
+            app.Map("/postdata", appBuilder =>
+            {
+                appBuilder.Run(httpContext =>
+                {
+                    var cookies = string.Join(", ", httpContext.Request.Cookies.Select(kvp => $"{kvp.Key} : {kvp.Value}"));
+
+                    return httpContext.Response.WriteAsync("Postdata action received the following cookies:" + cookies);
+                });
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseServer("Microsoft.AspNetCore.Server.Kestrel")
+                .UseDefaultConfiguration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ coreclrhttpclient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SendRequestsAsync().Wait();
+        }
+
+        private static async Task SendRequestsAsync()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5000/");
+            client.Timeout = TimeSpan.FromSeconds(5);
+
+            Console.WriteLine("Making a request to get the antiforgery cookie from server");
+            var response = await client.GetAsync("getdata");
+            Console.WriteLine("Response headers: " + Environment.NewLine + response.ToString());
+
+
+            Console.WriteLine("Posting a request to the server");
+            response = await client.PostAsync("postdata", new StringContent("asdfasdf"));
+            Console.WriteLine("Response headers: " + Environment.NewLine + response.ToString());
+            Console.WriteLine("Response content:");
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
